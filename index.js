6f68cb9de111e4ef029b56445d3c54160cadb13c
hugodelgado <hugo.delgado1@outlook.com>
2020-11-23 09:33:11 -0600

    commit tree 723c36deb7d27a107a16735acec77826c766a917

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+// let names = ["Lisa", "Kaitlin", "Jan"]
+// let event = ["surprise"]
+
+function writeCards (names, event) {
+    const array = []
+    for(let i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        // console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return array
+}
+
+function countDown(num) {
+    for (let i = num; i > -1; i--) {
+        console.log(i)
+    }
+}
+  
